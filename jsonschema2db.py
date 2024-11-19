ca67f7ff32e81c4b2062c5eca5c52390f7250b41
hollow2003 <1175010417@qq.com>
2024-11-19 00:47:20 -0800

    now can predeal the storage data,and fix some problem about name table

--- jsonschema2db.py
+++ jsonschema2db.py
@@ -1,8 +1,10 @@
-from sqlalchemy import Column, Float, Integer, String, Boolean, ForeignKey
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Float, Integer, String, Boolean, Enum, DateTime, ForeignKey
+from sqlalchemy.orm import declarative_base, composite
 from jsonschema import Draft7Validator
 from collections import deque
 from jsonschema.exceptions import SchemaError
+from sqlalchemy.dialects.postgresql import ARRAY, JSONB
+import enum
 
 
 Base = declarative_base()
@@ -17,8 +19,7 @@ class JSONSchemaToPostgres():
         else:
             self.root_table_name = root_table_name
         self.validate_schema()
-        self.required_fields = {}
-        self.orms = self.generate_orm()
+        self.orms = self.generate_basic_orm()
 
     def validate_schema(self):
         try:
@@ -28,120 +29,15 @@ class JSONSchemaToPostgres():
         except SchemaError as e:
             print(f"Schema 无效: {e.message}")
 
-    # def schema_to_tree(self, schema=None, parent_name=None, required=None):
-    #     # 不支持 oneof 或 allof
-    #     if schema is None:
-    #         schema = self.schema
-    #     if parent_name is None:
-    #         parent_name = "root"
-    #     if required is None:
-    #         required = []
-    #     if schema["type"] == "object":
-    #         properties_tree = {}
-    #         for prop_name, prop_schema in schema['properties'].items():
-    #             properties_tree[prop_name] = self.schema_to_tree(prop_schema, prop_name, schema.get("required"))
-    #         return properties_tree
-    #     elif schema["type"] == "array":
-    #         items_tree = []
-    #         if isinstance(schema.get("items"), dict):
-    #             items_tree.append(self.schema_to_tree(schema['items'], parent_name, schema.get("required")))
-    #             return items_tree
-    #         elif isinstance(schema.get("items"), list):
-    #             for item_schema in schema['items']:
-    #                 items_tree.append(self.schema_to_tree(item_schema, parent_name, schema.get("required")))
-    #             return items_tree
-    #     elif "type" in schema:
-    #         if parent_name in required:
-    #             return {"type": schema["type"], "required": True}
-    #         else:
-    #             return {"type": schema["type"], "required": False}
-    #     return None
-
-    # def print_tree(self, tree=None, indent=0):
-    #     if tree is None:
-    #         tree = self.schema_tree
-    #     # 如果树是字典类型
-    #     if isinstance(tree, dict):
-    #         for key, value in tree.items():
-    #             print(' ' * indent + f"{key}:")
-    #             self.print_tree(value, indent + 2)  # 缩进增加，表示层级
-    #     # 如果树是列表类型
-    #     elif isinstance(tree, list):
-    #         for i, item in enumerate(tree):
-    #             print(' ' * indent + f"Item {i}:")
-    #             self.print_tree(item, indent + 2)
-    #     # 否则，打印值
-    #     else:
-    #         print(' ' * indent + str(tree))  # 这里是正确的：打印树节点的值
-
-    # def generate_orm(self):
-    #     # 只允许结构体数组(items 为字典，可以包含其他基本类型)，最外层只为结构体
-    #     # 如果为"items":[]则里面只能为基本类型
-    #     queue = deque([(self.schema, self.root_table_name, None)])
-    #     orm_classes = {}
-    #     while queue:
-    #         columns = {}
-    #         sub_nodes = []
-    #         current_schema, table_name, parent_table = queue.popleft()
-    #         if current_schema.get("type") == "object":  # 如果是对象类型
-    #             for key, value in current_schema.get("properties", {}).items():
-    #                 if value.get("type") == "object" or value.get("type") == "array":
-    #                     # 递归处理嵌套的 object 类型
-    #                     sub_table_name = f"{table_name}_{key}"
-    #                     sub_nodes.append((value, sub_table_name))
-    #                 else:
-    #                     # 处理基本类型
-    #                     columns[key] = Column(key, self.get_basic_type(value.get("type")))
-    #             if columns:
-    #                 columns["id"] = Column("id", Integer, primary_key=True)
-    #                 if parent_table is not None:
-    #                     columns[f"{parent_table}_id"] = ForeignKey(f"{parent_table}.id")
-    #                     orm_classes[parent_table + "_" + table_name] = self.create_orm_class(parent_table + "_" + table_name, columns)
-    #                 else:
-    #                 # 创建 ORM 类
-    #                     orm_classes[table_name] = self.create_orm_class(table_name, columns)
-
-    #                 # 将子节点加入队列
-    #                 for sub_node, sub_table_name in sub_nodes:
-    #                     queue.append((sub_node, sub_table_name, table_name))
-    #             else:
-    #                 for sub_node, sub_table_name in sub_nodes:
-    #                     queue.append((sub_node, sub_table_name, None))
-
-    #         elif current_schema.get("type") == "array":  # 如果是数组类型
-
-    #             if type(current_schema.get("items")) == list:
-    #                 index = 0
-    #                 for item in current_schema.get("items", []):
-    #                     columns[table_name + "_item_" + str(index)] = Column(table_name + "_item_" + str(index), self.get_basic_type(item.get("type")), nullable=False)
-    #                     index += 1
-    #             else:
-    #                 if "type" in current_schema.get("items"):
-    #                     if current_schema.get("items").get("type") == "object" or current_schema.get("items").get("type") == "array":
-    #                         sub_table_name = table_name
-    #                         sub_nodes.append((current_schema.get("items"), sub_table_name))
-    #             if columns:
-    #                 columns["id"] = Column("id", Integer, primary_key=True)
-    #                 if parent_table is not None:
-    #                     columns[f"{parent_table}_id"] = ForeignKey(f"{parent_table}.id")
-    #                     orm_classes[parent_table + "_" + table_name] = self.create_orm_class(parent_table + "_" + table_name, columns)
-    #                 else:
-    #                 # 创建 ORM 类
-    #                     orm_classes[table_name] = self.create_orm_class(table_name, columns)
-    #                 # 将子节点加入队列
-    #                 for sub_node, sub_table_name in sub_nodes:
-    #                     queue.append((sub_node, sub_table_name, table_name))
-    #             else:
-    #                 for sub_node, sub_table_name in sub_nodes:
-    #                     queue.append((sub_node, sub_table_name, None))
-    #     return orm_classes
-
-    def generate_orm(self):
-        # 初始化队列进行层序遍历，存储表格及其父表的相关信息
-        # 不支持 oneof 或 allof
-        # 如果"items" :[]，则[]内只能为基础类型
-        # 如果最外层为array，且"items" : {}，为结构体列表，则会先产生一个root_table_name只包含id，
-        # 还有一个root_table_root_table_item包含结构体
+    def generate_basic_orm(self):
+        # 初始化队列进行层序遍历，存储表格及其父表的相关信息，仅使用基本类型
+        # 以下规则中，非基本类型指的是 array 或 object
+        # 规则1：array 的 items 只有两种形式，一种为结构体数组，一种为指定位置为基本类型的数组
+        # 规则2：禁止使用id关键字
+        # 规则3：不支持 oneof 或 allof 以及 additionalProperties
+        # 生成规则1：如果最外层一定会生成一个表root_table_name
+        # 生成规则2：如果为结构体数组，则不会生成对应的表，而是生成table_name_item作为存储结构体的表
+        # 生成规则3：如果结构体内没有基本类型，则不会生成table_name，而是继续处理内部的非基本类型
         node_queue = deque([(self.schema, self.root_table_name, None)])
         orm_classes = {}
 
@@ -185,10 +81,15 @@ class JSONSchemaToPostgres():
                 sub_table_name = f"{table_name}_{key}"
                 sub_nodes.append((value, sub_table_name))
             else:
+                if "enum" in value:
+                    column_type = self.handle_enum(key, value)
+                else:
+                    # 不是 enum 类型时，使用基本类型
+                    column_type = self.get_basic_type(value.get("type"))
                 columns[key] = Column(
                     key,
-                    self.get_basic_type(value.get("type")),
-                    nullable=(key not in current_schema.get("required"))
+                    column_type,
+                    nullable=("required" not in current_schema or key not in current_schema.get("required"))
                 )
 
         self.create_and_add_orm_class(
@@ -208,9 +109,14 @@ class JSONSchemaToPostgres():
         if isinstance(items_schema, list):
             index = 0
             for item in items_schema:
+                if "enum" in item:
+                    column_type = self.handle_enum(f"{table_name}_item_{index}", item)
+                else:
+                    # 不是 enum 类型时，使用基本类型
+                    column_type = self.get_basic_type(item.get("type"))
                 columns[f"{table_name}_item_{index}"] = Column(
                     f"{table_name}_item_{index}",
-                    self.get_basic_type(item.get("type")),
+                    column_type,
                     nullable=False
                 )
                 index += 1
@@ -239,12 +145,12 @@ class JSONSchemaToPostgres():
             # 如果有父表，添加外键
             if parent_table:
                 columns[f"{parent_table}_id"] = ForeignKey(
-                    f"{self.root_table_name}.id"
+                    f"{parent_table}.id"
                 )
                 orm_classes[
-                    f"{parent_table}_{table_name}"
+                    table_name
                 ] = self.create_orm_class(
-                    f"{parent_table}_{table_name}", columns
+                    table_name, columns
                 )
             else:
                 orm_classes[table_name] = self.create_orm_class(
@@ -257,7 +163,7 @@ class JSONSchemaToPostgres():
                 node_queue.append((
                     sub_node,
                     sub_table_name,
-                    self.root_table_name)
+                    table_name)
                 )
 
         else:
@@ -271,7 +177,7 @@ class JSONSchemaToPostgres():
                 node_queue.append((
                     sub_node,
                     sub_table_name,
-                    self.root_table_name
+                    parent_table
                     )
                 )
 
@@ -285,6 +191,27 @@ class JSONSchemaToPostgres():
         }
         return type_mapping.get(schema_item, String)  # 默认使用 String 类型
 
+    def handle_enum(self, key, value):
+        """
+        Handles enum types.
+        """
+        enum_values = value.get("enum", [])
+        enum_type = value.get("type", "string")  # Default to string type for enum
+
+        if enum_type == "string":
+            EnumType = enum.Enum(f"{key.capitalize()}_Enum", enum_values, type=str)
+        elif enum_type == "integer":
+            # If enum is of integer type
+            enum_values = [int(v) for v in enum_values]
+            EnumType = enum.Enum(f"{key.capitalize()}_Enum", enum_values, type=int)
+        elif enum_type == "number":
+            # If enum is of number type
+            enum_values = [float(v) for v in enum_values]
+            EnumType = enum.Enum(f"{key.capitalize()}_Enum", enum_values, type=float)
+
+        # Return SQLAlchemy Enum type
+        return Enum(EnumType)
+
     def create_orm_class(self, table_name, columns):
         """根据字段创建 ORM 类"""
         print(table_name)
@@ -293,6 +220,49 @@ class JSONSchemaToPostgres():
             '__tablename__': table_name,
             '__classname__': table_name
         }
-        for column_name, column_type in columns.items():
-            attrs[column_name] = column_type
+        for column_name, column in columns.items():
+            attrs[column_name] = column
         return type(table_name, (Base,), attrs)
+
+    def flatten_dict(self, data):
+        result = []
+        stack = [(data, self.root_table_name, None)]
+        index = 0
+        while stack:
+            current, table_name, parent_index = stack.pop()
+            if isinstance(current, dict):
+                flattened = {table_name: {}}
+                for key, value in current.items():
+                    if isinstance(value, dict):
+                        # 如果值是字典，则将该字典继续添加到队列中
+                        stack.append((value, table_name + '_' + key, index))
+                    elif isinstance(value, list):
+                        # 如果值是列表，则将每个元素（可能是字典或其他）继续添加到队列中
+                        stack.append((value, table_name + '_' + key, index))
+                    else:
+                        # 如果值是基本类型，直接添加到当前扁平化字典
+                        flattened[table_name][key] = value
+                if flattened != {}:
+                    # 将当前层的扁平字典添加到结果中
+                    if parent_index:
+                        flattened[table_name]["parent_index"] = parent_index
+                    result.append(flattened)
+                    index += 1
+            elif isinstance(current, list):
+                flattened = {}
+                temp = []
+                for item in current:
+                    if isinstance(item, dict) or isinstance(item, list):
+                        # 如果值是字典，则将该字典继续添加到队列中
+                        stack.append((item, table_name + "_item", index))
+                    else:
+                        temp.append(item)
+                if temp != []:
+                    flattened[table_name] = temp
+                if flattened != {}:
+                    # 将当前层的扁平字典添加到结果中
+                    if parent_index:
+                        flattened[table_name]["parent_index"] = parent_index
+                    result.append(flattened)
+                    index += 1
+        return result
--- remote_logger.py
+++ remote_logger.py
@@ -11,19 +11,47 @@ logger = logging.getLogger()
 if __name__ == '__main__':
     # 加载 JSON schema
     schema = {
-  "type": "array",
-  "items": {
-    "type": "object",
-    "properties": {
-      "name": {
-        "type": "string"
-      },
-      "age": {
-        "type": "integer"
-      }
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "type": "object",
+  "properties": {
+    "studentId": {
+      "type": "string"
     },
-    "required": ["name", "age"]
-  }
+    "name": {
+      "type": "string"
+    },
+    "courses": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+          "courseId": {
+            "type": "string"
+          },
+          "courseName": {
+            "type": "string"
+          },
+          "grades": {
+            "type": "array",
+            "items": {
+              "type": "object",
+              "properties": {
+                "examType": {
+                  "type": "string"
+                },
+                "grade": {
+                  "type": "string"
+                }
+              },
+              "required": ["examType", "grade"]
+            }
+          }
+        },
+        "required": ["courseId", "courseName", "grades"]
+      }
+    }
+  },
+  "required": ["studentId", "name", "courses"]
 }
 
 
@@ -33,7 +61,39 @@ if __name__ == '__main__':
 
 
 
-
-
     json_to_pg = JSONSchemaToPostgres("localhost", schema, "root_table")
+    print(json_to_pg.flatten_dict({
+  "studentId": "S12345",
+  "name": "Alice",
+  "courses": [
+    {
+      "courseId": "C001",
+      "courseName": "Mathematics",
+      "grades": [
+        {
+          "examType": "Midterm",
+          "grade": "A"
+        },
+        {
+          "examType": "Final",
+          "grade": "B+"
+        }
+      ]
+    },
+    {
+      "courseId": "C002",
+      "courseName": "Physics",
+      "grades": [
+        {
+          "examType": "Midterm",
+          "grade": "B"
+        },
+        {
+          "examType": "Final",
+          "grade": "A"
+        }
+      ]
+    }
+  ]
+}))
     # {'loginfo': {'functionName': 'string', 'checkpointName': 'string', 'logLevel': 'string'}, 'Position': {'x': 'number', 'y': 'number', 'raw': 'number'}}
